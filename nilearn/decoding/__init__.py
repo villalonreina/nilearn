b8b2825666c4a3fd229c76609cda94afd561d72c
Julio Villalon <neurobarranquilla@hotmail.com>
2016-03-11 16:53:21 -0800

    changed init file in decoding to its original format

--- nilearn/decoding/__init__.py
+++ nilearn/decoding/__init__.py
@@ -2,7 +2,7 @@
 Decoding tools and algorithms.
 """
 
-#from .searchlight import SearchLight
-#from .space_net import SpaceNetClassifier, SpaceNetRegressor
-#
-#__all__ = ['SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor']
+from .searchlight import SearchLight
+from .space_net import SpaceNetClassifier, SpaceNetRegressor
+
+__all__ = ['SearchLight', 'SpaceNetClassifier', 'SpaceNetRegressor']
